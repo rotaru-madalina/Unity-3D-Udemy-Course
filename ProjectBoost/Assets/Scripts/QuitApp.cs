9536bcce62df78e2d5033b525955be22d9eba067
Marcu Ionut <r.madalina0@gmail.com>
2022-11-22 23:31:33 +0200

    added quit escape app

---
+++ ProjectBoost/Assets/Scripts/QuitApp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApp : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            //Debug.Log("Am iesit yay \n");
+        }
+    }
+}
